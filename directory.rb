ba2d6d89d6e7894ba95abea118a27772777d88d0
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-02 13:32:36 +0000

    Implement user input to get student names
    Rather than names being hard-coded, the script now prompts the user for
    a list of names, each entered separately until an empty string is
    entered.

--- directory.rb
+++ directory.rb
@@ -1,16 +1,20 @@
-students = [
-  { name: "Dr. Hannibal Lector", cohort: :november },
-  { name: "Darth Vader", cohort: :november },
-  { name: "Nurse Ratched", cohort: :november },
-  { name: "Michael Corleone", cohort: :november },
-  { name: "Alex DeLarge", cohort: :november },
-  { name: "The Wicked Witch of the West", cohort: :november },
-  { name: "Terminator", cohort: :november },
-  { name: "Freddy Krueger", cohort: :november },
-  { name: "The Joker", cohort: :november },
-  { name: "Joffrey Baratheon", cohort: :november },
-  { name: "Norman Bates", cohort: :november }
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  # take names until the user inputs an empty name
+  while !name.empty? do
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  # return students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +31,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
