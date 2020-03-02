f758fa1a6a8d4ef1cbdc314d070f39a0f07d5b9c
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-02 11:42:25 +0000

    Continue refactoring directory.rb
    An iterator is now used to print the array of students.
    
    The number of students printed in the summary now comes from the length
    of the student array, so the student_count variable has been deleted as
    it is no longer necessary.

--- directory.rb
+++ directory.rb
@@ -1,20 +1,25 @@
-student_count = 11
+# let's put all students into an array
+students = [
+  "Dr. Hannibal Lector",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
-# Print a list of students
+# and then print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lector"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
 
-# Print a summary of the total number
-# Note we use print rather than puts as it doesn't add \n
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+
+# finally, we print the total
+puts "Overall, we have #{students.count} great students"
