6374c3b99c78c5daa1ed0aba836043ca953f6879
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-02 13:45:52 +0000

    Ask user for a letter to filter the list with
    The script now prompts the user for a letter to filter the list of
    student names with. The filtering itself is not yet implemented.

--- directory.rb
+++ directory.rb
@@ -16,6 +16,22 @@ def input_students
   students
 end
 
+def input_filters
+  filter_set = false
+  puts "Please input a starting letter to filter the names by (press enter to ignore)"
+
+  while !filter_set do
+    letter_filter = gets.chomp
+
+    # only return if the filter is "" or a single character
+    if letter_filter.length < 2
+      return letter_filter
+    else
+      puts "The filter must be either empty or a single character"
+    end
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -32,6 +48,7 @@ def print_footer(names)
 end
 
 students = input_students
+filters = input_filters
 print_header
 print(students)
 print_footer(students)
