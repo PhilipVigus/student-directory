e0b9ef25e5746e33399fabd7fb8edde404b1ffaf
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-03 15:06:13 +0000

    Continue refactoring script
    The script was refactored, splitting large methods into smaller
    submethods.

--- directory.rb
+++ directory.rb
@@ -1,25 +1,35 @@
+# instance variable on the global instance?
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-    # print the menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # get the user input
-    selection = gets.chomp
-    # do what they asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
@@ -27,18 +37,13 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
   name = gets.chomp
-
   # take names until the user inputs an empty name
   while !name.empty? do
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
+    @students << { name: name, cohort: :november }
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-
-  # return students
-  students
 end
 
 def print_header
@@ -46,14 +51,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
